25118c9e1105ad4320f7278f1966676df79985c8
Игорь Бесчастнов <30983257+IgorBeschastnov@users.noreply.github.com>
2018-12-20 15:05:06 +0300

    Update __init__.py

--- spockesman/states/__init__.py
+++ spockesman/states/__init__.py
@@ -2,8 +2,3 @@ from .awaiting_state import AwaitingState
 from .base import initial
 from .commands import *
 from .state import WrongCommandException, NoHandlerException, State
-
-
-def generate_states(data):
-    for state, config in data.items():
-        print(state, config)
