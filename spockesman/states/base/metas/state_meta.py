d7e4dd9d13cbded6d4aa37d562d24ada061d515b
Igor Beschastnov <beschastnovigor@gmail.com>
2019-01-22 20:05:49 +0300

    Reworked 'transform' attributes.

--- spockesman/states/base/metas/state_meta.py
+++ spockesman/states/base/metas/state_meta.py
@@ -1,9 +1,14 @@
 from copy import deepcopy
 
 from .environment import STATES
-
+from .exceptions import ConstantViolationException
 
 class StateMeta(type):
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__.get('__const_list', {}):
+            raise ConstantViolationException()
+        super().__setattr__(name, value)
 
     def __new__(mcs, name, bases, dct):
         try:
@@ -16,23 +21,9 @@ class StateMeta(type):
             last_transform = tuple()
 
         transform = tuple({*transform, *last_transform})
+        
         dct['transform'] = transform
-
-        def property_gen(requested_key):
-            try:
-                item = dct[requested_key]
-            except KeyError:
-                return
-
-            def act_prop():
-                # TODO: this is pretty slow, try to find another way
-                return deepcopy(item)
-            return item, act_prop
-
-        for key in transform:
-            attr_pair = property_gen(key)
-            if attr_pair:
-                dct[f"__{key}"], dct[key] = attr_pair[0], attr_pair[1]
+        dct['__const_list'] = [*transform]
 
         cls = super().__new__(mcs, name, bases, dct)
         STATES[name] = cls
