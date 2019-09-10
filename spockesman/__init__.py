478a39a0565dca1631e17f40f9a3adf922b01d7b
Beschastnov Igor Vladimirovich <i.beschastnov@tcsbank.ru>
2019-09-10 17:12:41 +0300

    TODOs

--- spockesman/__init__.py
+++ spockesman/__init__.py
@@ -7,7 +7,12 @@ from spockesman.results import ABCResult
 
 __version__ = '0.1.3'
 
-# TODO: refactor relative imports, change everything to absolute imports
 # TODO: move to pytest from unittest
 # TODO: do we really need to do 'user_input, call_args = args[:2]' everywhere?
-#  Do we even need args and kwargs in state's __call__?
+#  Do we even need *args and **kwargs in state's __call__?
+# TODO: write more tests
+# TODO: remove global variables!
+# TODO: generate config file from loaded state machine
+# TODO: setup linters
+# TODO: setup CI
+# TODO: setup code-coverage checks
--- spockesman/states/__init__.py
+++ spockesman/states/__init__.py
@@ -3,9 +3,3 @@ from spockesman.states.commands import *
 from spockesman.states.transient_state import TransientState
 from spockesman.states.cyclic_state import CyclicState
 from spockesman.states.state import InvalidCommandException, NoHandlerException, State
-
-
-# TODO: generate config file from loaded state machine
-# TODO: setup linters
-# TODO: setup CI
-# TODO: setup code-coverage checks
