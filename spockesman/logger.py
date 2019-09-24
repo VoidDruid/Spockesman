829235cf8f1bbdc0339d179c72247531e08c8e49
Beschastnov Igor Vladimirovich <i.beschastnov@tcsbank.ru>
2019-09-24 14:59:04 +0300

    Change logger level

--- spockesman/logger.py
+++ spockesman/logger.py
@@ -1,6 +1,6 @@
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    level=logging.INFO, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 )
 log = logging.getLogger('Spockesman')
